aa7c514925ca46f6d05793876a15e014c08e7409
Mauno Elo <mauser83@hotmail.com>
2025-08-01 15:17:35 +0300

    split tsconfig for testing and build

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,29 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  preset: "ts-jest",
+  testEnvironment: "node",
+  globals: {
+    "ts-jest": {
+      tsconfig: "tsconfig.test.json",
+    },
+  },
   modulePathIgnorePatterns: ["<rootDir>/dist/"],
-  globalSetup: '<rootDir>/__tests__/globalSetup.ts',
-  globalTeardown: '<rootDir>/__tests__/globalTeardown.ts',
-  setupFilesAfterEnv: ['<rootDir>/__tests__/testUtils.ts'],
+  globalSetup: "<rootDir>/__tests__/globalSetup.ts",
+  globalTeardown: "<rootDir>/__tests__/globalTeardown.ts",
+  setupFilesAfterEnv: ["<rootDir>/__tests__/testUtils.ts"],
   testPathIgnorePatterns: [
-    '<rootDir>/__tests__/globalSetup.ts',
-    '<rootDir>/__tests__/globalTeardown.ts',
-    '<rootDir>/__tests__/testUtils.ts'
+    "<rootDir>/__tests__/globalSetup.ts",
+    "<rootDir>/__tests__/globalTeardown.ts",
+    "<rootDir>/__tests__/testUtils.ts",
   ],
   transform: {
-    '^.+\\.ts?$': ['ts-jest', { 
-      tsconfig: 'tsconfig.test.json',
-      isolatedModules: true
-    }],
+    "^.+\\.ts?$": [
+      "ts-jest",
+      {
+        tsconfig: "tsconfig.test.json",
+        isolatedModules: true,
+      },
+    ],
   },
   testTimeout: 30000,
   maxWorkers: 1, // Run tests serially to avoid port conflicts
